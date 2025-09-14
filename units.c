9dbfd3fd03d4d425c231d9178d18164341394744
okutima <sandrakush776@gmail.com>
2025-09-14 19:52:30 +0300

    Create units.c

---
+++ units.c
@@ -0,0 +1,18 @@
+//include displaying all units done by software engineering students 
+/*Name:Sandra Okutima kuya
+Reg:PA106/G/28757/25*/
+
+#include<stdio.h> 
+
+int main(){
+printf("UNITS FOR SOFTWARE ENGINEERERS\n");
+printf("computer system organisation \n");
+printf("discrete mathematics\n");
+printf("communicationskills\n");
+printf("Fundamentalof ict\n");
+printf("introductionto programming\n");
+printf("mathematics for science\n");
+printf("systems and analysis\n");
+}
+
+
