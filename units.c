7fe4f423aa764408677d827dddde816623a9bc30
okutima <sandrakush776@gmail.com>
2025-09-14 19:55:32 +0300

    Update units.c

--- units.c
+++ units.c
@@ -5,14 +5,15 @@ Reg:PA106/G/28757/25*/
 #include<stdio.h> 
 
 int main(){
-printf("UNITS FOR SOFTWARE ENGINEERERS\n");
-printf("computer system organisation \n");
-printf("discrete mathematics\n");
-printf("communicationskills\n");
-printf("Fundamentalof ict\n");
-printf("introductionto programming\n");
-printf("mathematics for science\n");
-printf("systems and analysis\n");
+   printf("UNITS FOR SOFTWARE ENGINEERERS\n");
+   printf("computer system organisation \n");
+   printf("discrete mathematics\n");
+   printf("communicationskills\n");
+   printf("Fundamentalof ict\n");
+   printf("introductionto programming\n");
+   printf("mathematics for science\n");
+   printf("systems and analysis\n");
+   return 0;
 }
 
 
