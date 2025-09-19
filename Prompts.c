a91fca58487d1f2f36c1322d241cbdb50fd1deec
okutima <sandrakush776@gmail.com>
2025-09-19 23:01:51 +0300

    Add files via upload

---
+++ Prompts.c
@@ -0,0 +1,33 @@
+//Prompting height,bank balance and Id number
+/*
+Name:SANDRA KUYA OKUTIMA
+Reg no:PA106/G/28757/25
+Description:Prompting height,bank balance and Id number
+*/
+#include<stdio.h>
+int main(){
+	int height;
+	int bank_balance;
+	int Id_number;
+	int phone_number;
+	
+	printf("Enter your height: ");
+	scanf("%d",&height);
+	printf("Your height is %d\n",height);
+	
+	printf("Enter your bank_balance: ");
+	scanf("%d",&bank_balance);
+	printf("Your bank_balance is %d\n",bank_balance);
+	
+	printf("Enter your Id_number: ");
+	scanf("%d",&Id_number);
+	printf("Your Id_number is %d\n",Id_number);
+	
+	printf("Enter your phone_number: ");
+	scanf("%d",&phone_number);
+	printf("Your phone_number is %d\n",phone_number);
+	
+	return 0;
+	}
+	
+	
