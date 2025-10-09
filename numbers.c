0bc43d4abffdc487bc62ba7366a5ebb66b632625
okutima <sandrakush776@gmail.com>
2025-10-09 19:19:47 +0300

    Add files via upload

---
+++ numbers.c
@@ -0,0 +1,14 @@
+/*
+NAME:OKUTIMA SANDRA KUYA
+REG NO:PA106/G/28757/25
+DESCRIPTION:PRINTING NUMBERS FROM 100-50 USING FOR LOOP
+*/
+
+#include<stdio.h>
+int main(){
+int i;
+for(i=100;i>=50;i--)
+printf("%d\n",i);
+return 0;
+}
+    
